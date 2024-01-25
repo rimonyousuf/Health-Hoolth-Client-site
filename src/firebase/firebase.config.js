805a183993e97d310c31d9a908404fd5cb16bbf0
Rimon <rimonyousuf00@gmail.com>
2024-01-25 10:34:49 +0600

    Firebase config has been updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,23 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export default app;
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVlEJeXTMvBu6xXhpI0mmnrPlA1nbqZC8",
-  authDomain: "health-hoolth.firebaseapp.com",
-  projectId: "health-hoolth",
-  storageBucket: "health-hoolth.appspot.com",
-  messagingSenderId: "268338643947",
-  appId: "1:268338643947:web:327eab8c0b712740184fae"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
